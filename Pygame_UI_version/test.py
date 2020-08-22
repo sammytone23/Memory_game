c6e3fc903f807b8923af22f32d25e93a083e757f
sammytone23 <sammytone23@gmail.com>
2020-08-22 23:11:45 +1000

    got the test working finally

--- Pygame_UI_version/test.py
+++ Pygame_UI_version/test.py
@@ -10,24 +10,28 @@ WIDTH=640
 
 pygame.init()
 
-pygame.display.set_caption('Test')
-window_surface = pygame.display.set_mode((WIDTH, HEIGHT))
 
-background = pygame.Surface((WIDTH, HEIGHT))
-background.fill(pygame.Color('#5E6059'))
-
-manager = pygame_gui.UIManager((WIDTH, HEIGHT),'theme.json')
-
-manager.add_font_paths('Mono','mono.ttf')
 
 def main():
+  pygame.display.set_caption('Test')
+  window_surface = pygame.display.set_mode((WIDTH, HEIGHT))
+
+  background = pygame.Surface((WIDTH, HEIGHT))
+  background.fill(pygame.Color('#5E6059'))
+
+  manager = pygame_gui.UIManager((WIDTH, HEIGHT), 'theme.json')
+  manager.add_font_paths('Mono','mono.ttf')
+  
+  clock = pygame.time.Clock()
+
   test_btn_dims=(200,100)
-  test_btn=btn(relative_rect = pygame.Rect((-test_btn_dims[0] // 2 , test_btn_dims[1] // 2) , test_btn_dims),
+  test_btn=btn(relative_rect = pygame.Rect((WIDTH // 2 - test_btn_dims[0] // 2 , HEIGHT // 2 - test_btn_dims[1] // 2) , test_btn_dims),
                 text = 'Test',
                 manager=manager)
 
   end=False
   while not end:
+    time_delta = clock.tick(60)/1000.0
     for event in pygame.event.get(): 
       if event.type==pygame.QUIT:
         end=True
@@ -35,6 +39,7 @@ def main():
 
       manager.process_events(event)
 
+    manager.update(time_delta)
     window_surface.blit(background, (0, 0))
     manager.draw_ui(window_surface)
 
